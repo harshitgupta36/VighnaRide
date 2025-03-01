d896ff26ea3b398b493b694decb4927c6692be8e
harshitgupta36 <harshitgupta4799@gmail.com>
2025-03-01 11:16:53 +0530

    Initial Commit

---
+++ vighnarideCustomer/vighnarideCustomerApp.swift
@@ -0,0 +1,17 @@
+//
+//  vighnarideCustomerApp.swift
+//  vighnarideCustomer
+//
+//  Created by Harshit Gupta on 01/03/25.
+//
+
+import SwiftUI
+
+@main
+struct vighnarideCustomerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
